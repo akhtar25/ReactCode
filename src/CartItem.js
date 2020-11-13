b9ee858b8f0fcf9da74a79e9145ca02d65afb34a
Zaheen Akhtar <zaheen@alllearn.in>
2020-11-13 18:45:29 +0530

    add state to cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 class CartItem extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            title: 'Redmi',
+            price:'100,000',
+            quantity:'1',
+            img:'https://alllearndatabucketv2.s3.amazonaws.com/courseVideo/paul-blenkhorn-sensoryarthouse-6_e0KqpKkYw-unsplash.jpg'
+        }
+    }
     render(){
         return(
             <div className="cartItem">
                 <div className="cartLeft">
-                    <img style={styles.images} src="https://alllearndatabucketv2.s3.amazonaws.com/courseVideo/paul-blenkhorn-sensoryarthouse-6_e0KqpKkYw-unsplash.jpg"></img>
+                    <img style={styles.images} src={this.state.img}></img>
                 </div>
                 <div className="cartRight">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{color:'#777'}}>Rs. {this.state.price}</div>
+                    <div style={{color:'#777'}}>Qty: {this.state.quantity}</div>
                     <div className="actions">
-
+                        <img alt="increase quantity" className="actionIcons" src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"/> 
+                        <img alt="decrease quantity" className="actionIcons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"/>
+                        <img alt="delete product" className="actionIcons" src="https://www.flaticon.com/svg/static/icons/svg/3096/3096687.svg"/>
                     </div>
                 </div> 
             </div>
